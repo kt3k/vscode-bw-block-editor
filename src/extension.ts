702edc1905392b7b298d65d4573040beb32679d3
Yoshiya Hinosawa <stibium121@gmail.com>
2024-12-03 01:02:30 +0900

    chore: clean up

--- src/extension.ts
+++ src/extension.ts
@@ -32,28 +32,47 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
     webviewPanel: vscode.WebviewPanel,
     _token: vscode.CancellationToken,
   ): Promise<void> {
-    // Setup initial content for the webview
-    webviewPanel.webview.options = {
+    const { webview } = webviewPanel
+    webview.options = {
       enableScripts: true,
     }
-    webviewPanel.webview.html = this.#getHtml(
-      webviewPanel.webview,
-    )
+    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
+      this.#context.extensionUri,
+      "media",
+      "webview.js",
+    ))
+
+    const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
+      this.#context.extensionUri,
+      "media",
+      "style.css",
+    ))
+
+    webview.html = /* html */ `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${styleMainUri}" rel="stylesheet" />
+				<title>Cat Scratch</title>
+			</head>
+			<body>
+				<div class="notes">
+					<div class="add-button">
+						<button>Scratch!</button>
+					</div>
+				</div>
+				<script src="${scriptUri}"></script>
+			</body>
+			</html>`
 
     function updateWebview() {
-      webviewPanel.webview.postMessage({
+      webview.postMessage({
         type: "update",
         text: document.getText(),
       })
     }
-
-    // Hook up event handlers so that we can synchronize the webview with the text document.
-    //
-    // The text document acts as our model, so we have to sync change in the document to our
-    // editor and sync changes in the editor back to the document.
-    //
-    // Remember that a single text document can also be shared between multiple custom
-    // editors (this happens for example when you split a custom editor)
 
     const changeDocumentSubscription = vscode.workspace
       .onDidChangeTextDocument(
@@ -70,7 +89,7 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
     })
 
     // Receive message from the webview.
-    webviewPanel.webview.onDidReceiveMessage((e) => {
+    webview.onDidReceiveMessage((e) => {
       switch (e.type) {
         case "add":
           this.#addNewScratch(document)
@@ -83,58 +102,6 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
     })
 
     updateWebview()
-  }
-
-  #getHtml(webview: vscode.Webview): string {
-    // Local path to script and css for the webview
-    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this.#context.extensionUri,
-      "media",
-      "webview.js",
-    ))
-
-    const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this.#context.extensionUri,
-      "media",
-      "reset.css",
-    ))
-
-    const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this.#context.extensionUri,
-      "media",
-      "vscode.css",
-    ))
-
-    const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this.#context.extensionUri,
-      "media",
-      "style.css",
-    ))
-
-    return /* html */ `
-			<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-				<link href="${styleResetUri}" rel="stylesheet" />
-				<link href="${styleVSCodeUri}" rel="stylesheet" />
-				<link href="${styleMainUri}" rel="stylesheet" />
-
-				<title>Cat Scratch</title>
-			</head>
-			<body>
-				<div class="notes">
-					<div class="add-button">
-						<button>Scratch!</button>
-					</div>
-				</div>
-
-				<script src="${scriptUri}"></script>
-			</body>
-			</html>`
   }
 
   #addNewScratch(document: vscode.TextDocument) {
