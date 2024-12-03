6a115d9792e5b091da8cf9d1b6194079306122b8
Yoshiya Hinosawa <stibium121@gmail.com>
2024-12-04 00:49:37 +0900

    chore: clean up

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,33 @@
 import * as vscode from "vscode"
-
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.window.registerCustomEditorProvider(
     "kt3k.bwBlock",
     new BlockEdit(context),
   ))
 }
-
 class BlockEdit implements vscode.CustomTextEditorProvider {
   #uri: vscode.Uri
-
-  static #scratchCharacters = [
-    "😸",
-    "😹",
-    "😺",
-    "😻",
-    "😼",
-    "😽",
-    "😾",
-    "🙀",
-    "😿",
-    "🐱",
-  ]
-
+  static #chars = ["😸", "😹", "😺", "😻", "😼", "😽", "😾", "🙀", "😿", "🐱"]
   constructor(context: vscode.ExtensionContext) {
     this.#uri = context.extensionUri
   }
-
   async resolveCustomTextEditor(
     document: vscode.TextDocument,
     webviewPanel: vscode.WebviewPanel,
     _token: vscode.CancellationToken,
   ): Promise<void> {
     const { webview } = webviewPanel
-    webview.options = {
-      enableScripts: true,
-    }
+    webview.options = { enableScripts: true }
     const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
       this.#uri,
       "media",
       "webview.js",
     ))
-
     const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
       this.#uri,
       "media",
       "style.css",
     ))
-
     webview.html = /* html */ `
 			<!DOCTYPE html>
 			<html lang="en">
@@ -55,7 +35,6 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
 				<meta charset="UTF-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${styleMainUri}" rel="stylesheet" />
-				<title>Cat Scratch</title>
 			</head>
 			<body>
 				<div class="notes">
@@ -66,25 +45,11 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
 				<script src="${scriptUri}"></script>
 			</body>
 			</html>`
-
-    const changeDocumentSubscription = vscode.workspace
-      .onDidChangeTextDocument(
-        (e) => {
-          if (e.document.uri.toString() !== document.uri.toString()) return
-
-          webview.postMessage({
-            type: "update",
-            text: document.getText(),
-          })
-        },
-      )
-
-    // Make sure we get rid of the listener when our editor is closed.
-    webviewPanel.onDidDispose(() => {
-      changeDocumentSubscription.dispose()
+    const subscription = vscode.workspace.onDidChangeTextDocument((e) => {
+      if (e.document.uri.toString() !== document.uri.toString()) return
+      webview.postMessage({ type: "update", text: document.getText() })
     })
-
-    // Receive message from the webview.
+    webviewPanel.onDidDispose(() => subscription.dispose())
     webview.onDidReceiveMessage((e) => {
       switch (e.type) {
         case "add":
@@ -96,45 +61,26 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
           return
       }
     })
-
-    webview.postMessage({
-      type: "update",
-      text: document.getText(),
-    })
+    webview.postMessage({ type: "update", text: document.getText() })
   }
-
   #addNewScratch(document: vscode.TextDocument) {
     const json = JSON.parse(document.getText())
-    const character = BlockEdit
-      .#scratchCharacters[
-        Math.floor(
-          Math.random() *
-            BlockEdit.#scratchCharacters.length,
-        )
-      ]
+    const chars = BlockEdit.#chars
+    const c = chars[Math.floor(Math.random() * chars.length)]
     json.scratches = [
       ...(Array.isArray(json.scratches) ? json.scratches : []),
-      {
-        id: crypto.randomUUID(),
-        text: character,
-        created: Date.now(),
-      },
+      { id: crypto.randomUUID(), text: c, created: Date.now() },
     ]
-
     return this.#updateTextDocument(document, json)
   }
-
   #deleteScratch(document: vscode.TextDocument, id: string) {
     const json = JSON.parse(document.getText())
     if (!Array.isArray(json.scratches)) {
       return
     }
-
     json.scratches = json.scratches.filter((note: any) => note.id !== id)
-
     return this.#updateTextDocument(document, json)
   }
-
   #updateTextDocument(document: vscode.TextDocument, json: any) {
     const edit = new vscode.WorkspaceEdit()
     edit.replace(
