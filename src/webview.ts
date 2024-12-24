2333ec4734c51e5612d21e2cd0ed694a08e32db0
Yoshiya Hinosawa <stibium121@gmail.com>
2024-12-25 01:23:27 +0900

    chore: use cache, clean up

--- src/webview.ts
+++ src/webview.ts
@@ -4,38 +4,27 @@
 /// <reference types="npm:@types/vscode-webview" />
 
 import { BlockMap, TerrainBlock } from "@kt3k/bw/models"
+import { WeakValueMap } from "@kt3k/weak-value-map"
+import { type Context, mount, register } from "@kt3k/cell"
 
 const vscode = acquireVsCodeApi<{ uri: string; text: string }>()
 
 const notesContainer = document.querySelector<HTMLElement>(".notes")!
 
-const errorContainer = document.createElement("div")
-document.body.appendChild(errorContainer)
-errorContainer.className = "error"
-errorContainer.style.display = "none"
-
 async function updateContent(uri: string, text: string) {
-  try {
-    const blockMap = new BlockMap(uri, JSON.parse(text))
-    const terrainBlock = new TerrainBlock(blockMap, loadImage)
-    const canvas = await terrainBlock.createCanvas()
-    canvas.style.left = ""
-    canvas.style.top = ""
-    canvas.style.position = ""
-    const div = document.createElement("div")
-    div.innerHTML = `<div><div>${uri}</div><pre>${
-      JSON.stringify(blockMap, null, 2)
-    }</pre></div>`
-    notesContainer.appendChild(canvas)
-    notesContainer.appendChild(div)
-  } catch {
-    notesContainer.style.display = "none"
-    errorContainer.innerText = "Error: Document is not valid json"
-    errorContainer.style.display = ""
-    return
-  }
+  const blockMap = new BlockMap(uri, JSON.parse(text))
+  const terrainBlock = new TerrainBlock(blockMap, loadImage)
+  const canvas = await terrainBlock.createCanvas()
+  canvas.style.left = ""
+  canvas.style.top = ""
+  canvas.style.position = ""
+  const div = document.createElement("div")
+  div.innerHTML = `<div><div>${uri}</div><pre>${
+    JSON.stringify(blockMap, null, 2)
+  }</pre></div>`
+  notesContainer.appendChild(canvas)
+  notesContainer.appendChild(div)
   notesContainer.style.display = ""
-  errorContainer.style.display = "none"
 }
 
 window.addEventListener("message", (event) => {
@@ -55,13 +44,38 @@ window.addEventListener("message", (event) => {
   }
 })
 
-function loadImage(uri: string): Promise<HTMLImageElement> {
+function loadImage_(uri: string): Promise<HTMLImageElement> {
   return new Promise((resolve) => {
     const id = Math.random().toString()
     loadImageMap[id] = { resolve }
     vscode.postMessage({ type: "loadImage", uri, id })
   })
 }
+
+function memoizedLoading<K, A extends WeakKey>(
+  fn: (key: K) => Promise<A>,
+): (key: K) => Promise<A> {
+  const weakValueMap = new WeakValueMap<K, Promise<A>>()
+  const weakKeyMap = new WeakMap<A, Promise<A>>()
+  return (key: K) => {
+    const cache = weakValueMap.get(key)
+    if (cache) {
+      return cache
+    }
+    const promise = fn(key)
+    // Cache the promise by the key
+    weakValueMap.set(key, promise)
+    promise.then((value) => {
+      // Create a weak map from the asest to the loading promise.
+      // This prevents the promise from being garbage collected
+      // as long as the asset is being used
+      weakKeyMap.set(value, promise)
+    })
+    return promise
+  }
+}
+
+const loadImage = memoizedLoading(loadImage_)
 
 const loadImageMap: Record<
   string,
