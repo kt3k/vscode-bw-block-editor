a4fbb3d82bff870bfd9118a7acd7817ef35f9c71
Yoshiya Hinosawa <stibium121@gmail.com>
2025-01-19 20:21:29 +0900

    refactor: clean up

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,12 @@
 // @ts-types="@types/vscode"
 import * as vscode from "vscode"
 import { Buffer } from "node:buffer"
-import type { ExtensionMessage, WebviewMessage } from "./types.ts"
+import type {
+  ExtensionMessage,
+  WebviewMessage,
+  WebviewMessageLoadImage,
+  WebviewMessageUpdate,
+} from "./types.ts"
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.window.registerCustomEditorProvider(
@@ -12,7 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 class BlockEdit implements vscode.CustomTextEditorProvider {
   #uri: vscode.Uri
-  static #chars = ["😸", "😹", "😺", "😻", "😼", "😽", "😾", "🙀", "😿", "🐱"]
 
   constructor(context: vscode.ExtensionContext) {
     this.#uri = context.extensionUri
@@ -23,18 +27,53 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
     panel: vscode.WebviewPanel,
     _token: vscode.CancellationToken,
   ): Promise<void> {
-    const { webview } = panel
+    const webview = new CustomWebview(this.#uri, document, panel)
+    webview.onDidReceiveMessage(async (e: WebviewMessage) => {
+      if (e.type === "loadImage") {
+        this.#loadImage(e, webview)
+      } else if (e.type === "update") {
+        this.#update(document, e.map)
+      }
+    })
+  }
+
+  #update(document: vscode.TextDocument, e: WebviewMessageUpdate) {
+    const edit = new vscode.WorkspaceEdit()
+    edit.replace(
+      document.uri,
+      new vscode.Range(0, 0, document.lineCount, 0),
+      JSON.stringify(e.map, null, 2),
+    )
+    vscode.workspace.applyEdit(edit)
+  }
+
+  async #loadImage(e: WebviewMessageLoadImage, webview: CustomWebview) {
+    const uri = vscode.Uri.parse(e.uri)
+    const data = await vscode.workspace.fs.readFile(uri)
+    const base64 = Buffer.from(data).toString("base64")
+    const src = `data:image/png;base64,${base64}`
+    webview.postMessage({ type: "loadImageResponse", text: src, id: e.id })
+  }
+}
+
+class CustomWebview {
+  #webview: vscode.Webview
+  #document: vscode.TextDocument
+
+  constructor(
+    uri: vscode.Uri,
+    document: vscode.TextDocument,
+    panel: vscode.WebviewPanel,
+  ) {
+    this.#document = document
+    const webview = this.#webview = panel.webview
     webview.options = { enableScripts: true }
-    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this.#uri,
-      "out",
-      "webview.js",
-    ))
-    const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this.#uri,
-      "src",
-      "style.css",
-    ))
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(uri, "out/webview.js"),
+    )
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(uri, "src/style.css"),
+    )
     webview.html = /* html */ `
       <!DOCTYPE html>
       <html lang="en">
@@ -57,63 +96,27 @@ class BlockEdit implements vscode.CustomTextEditorProvider {
         </body>
       </html>
     `
-
-    const postMessage = (message: ExtensionMessage) =>
-      webview.postMessage(message)
-
-    const update = () => {
-      postMessage({
-        type: "update",
-        text: document.getText(),
-        uri: document.uri.toString(),
-      })
-    }
     const subscription = vscode.workspace.onDidChangeTextDocument((e) => {
       if (e.document.uri.toString() !== document.uri.toString()) return
-      update()
+      this.#updateWebview()
     })
     panel.onDidDispose(() => subscription.dispose())
-    webview.onDidReceiveMessage(async (e: WebviewMessage) => {
-      if (e.type === "loadImage") {
-        const uri = vscode.Uri.parse(e.uri)
-        const data = await vscode.workspace.fs.readFile(uri)
-        const base64 = Buffer.from(data).toString("base64")
-        const src = `data:image/png;base64,${base64}`
-        postMessage({ type: "loadImageResponse", text: src, id: e.id })
-      } else if (e.type === "update") {
-        this.#update(document, e.map)
-      }
+    this.#updateWebview()
+  }
+
+  postMessage(message: ExtensionMessage) {
+    this.#webview.postMessage(message)
+  }
+
+  #updateWebview() {
+    this.postMessage({
+      type: "update",
+      text: this.#document.getText(),
+      uri: this.#document.uri.toString(),
     })
-    update()
   }
 
-  #addNew(document: vscode.TextDocument) {
-    const json = JSON.parse(document.getText())
-    const chars = BlockEdit.#chars
-    const c = chars[Math.floor(Math.random() * chars.length)]
-    json.scratches = [
-      ...(Array.isArray(json.scratches) ? json.scratches : []),
-      { id: crypto.randomUUID(), text: c, created: Date.now() },
-    ]
-    return this.#update(document, json)
-  }
-
-  #delete(document: vscode.TextDocument, id: string) {
-    const json = JSON.parse(document.getText())
-    if (!Array.isArray(json.scratches)) {
-      return
-    }
-    json.scratches = json.scratches.filter((note: any) => note.id !== id)
-    return this.#update(document, json)
-  }
-
-  #update(document: vscode.TextDocument, json: any) {
-    const edit = new vscode.WorkspaceEdit()
-    edit.replace(
-      document.uri,
-      new vscode.Range(0, 0, document.lineCount, 0),
-      JSON.stringify(json, null, 2),
-    )
-    return vscode.workspace.applyEdit(edit)
+  onDidReceiveMessage(listener: (e: WebviewMessage) => void) {
+    this.#webview.onDidReceiveMessage(listener)
   }
 }
