3409d86cf45576d1b33dd9142face0bb388eea09
Yoshiya Hinosawa <stibium121@gmail.com>
2025-01-26 00:51:10 +0900

    update package

--- src/extension.ts
+++ src/extension.ts
@@ -33,7 +33,7 @@ function blockEditor(
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="${u("src/style.css")}" rel="stylesheet" />
+        <link href="${u("out/style.css")}" rel="stylesheet" />
         <style>
           canvas {
             image-rendering: crisp-edges;
